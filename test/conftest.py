3faa8e61676393f9b5a4596b9086c1206c665800
Parshwa <parshwapatil9@gmail.com>
2024-05-13 21:44:11 +0530

    Post Like Done

---
+++ test/conftest.py
@@ -0,0 +1,11 @@
+import pytest
+from selenium import webdriver
+
+@pytest.fixture(scope="module")
+def launch_browser():
+    driver = webdriver.Chrome()
+    driver.get('http://localhost:3000/')
+    driver.maximize_window()
+    driver.implicitly_wait(10)
+    yield driver
+    driver.quit()
